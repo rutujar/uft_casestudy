e601e41576da3061c49acd1c18c512437e5c637d
Rutuja Rajesh <rutujarajesh23@gmail.com>
2020-02-07 09:50:34 +0530

    Create login.vb

---
+++ cs9/login.vb
@@ -0,0 +1,3 @@
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("agentName").Set "john"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfEdit("password").SetSecure "5d7f1aee3d4615dfb793"
+WpfWindow("HPE MyFlight Sample Applicatio").WpfButton("OK").Click
